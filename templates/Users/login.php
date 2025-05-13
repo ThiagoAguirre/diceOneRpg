db24ef546ac2613a2f71416277c2fe6fdcd63704
Calicothico <thiago.aguirre08@outlook.com>
2025-05-13 00:23:48 -0300

    Enhance login page: add 'Remember me' checkbox, improve form labels, and update styles for better usability

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -9,15 +9,20 @@
     <!-- Right section for the form -->
     <div class="login-form">
         <div class="users form">
-            <h3>Login</h3>
+            <h3><?= __('Login') ?></h3>
             <?= $this->Form->create() ?>
             <fieldset>
                 <legend><?= __('Please enter your username and password') ?></legend>
-                <?= $this->Form->control('email', ['required' => true]) ?>
-                <?= $this->Form->control('password', ['required' => true]) ?>
+                <?= $this->Form->control('email', ['required' => true, 'label' => __('Email Address')]) ?>
+                <?= $this->Form->control('password', ['required' => true, 'label' => __('Password')]) ?>
+                <?= $this->Form->control('remember_me', ['type' => 'checkbox', 'label' => __('Remember me (7 days)')]) ?>
             </fieldset>
-            <?= $this->Form->submit(__('Login')); ?>
+            <?= $this->Form->submit(__('Login'), ['class' => 'btn btn-primary']); ?>
             <?= $this->Form->end() ?>
+
+            <div class="additional-links">
+                <?= $this->Html->link(__('I forgot my password!'), ['action' => 'register'], ['class' => 'register-link']) ?>
+            </div>
         </div>
     </div>
 </div>
